db9ea7e253fc0be8fc227d936948fae939bcad66
Julian <81520713+Juliaaan2502@users.noreply.github.com>
2021-07-03 19:13:27 +0200

    Add files via upload

--- app/src/main/java/eu/discordlistforbotsdevel/browser/MainActivity.java
+++ app/src/main/java/eu/discordlistforbotsdevel/browser/MainActivity.java
@@ -24,13 +24,13 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends Activity {
 
     EditText URLText;
-    Button GoButton;
-    // Button GoBack;
+    ImageFilterButton GoButton;
     WebView Browser;
     ImageFilterButton AppSettings;
+    ImageFilterButton Home;
 
 
-    String androidAppVersion = "0.11";
+    String androidAppVersion = "1.1";
 
     private class MyWebViewClient extends WebViewClient implements OnClickListener {
         @Override
@@ -52,10 +52,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         URLText = (EditText)findViewById(R.id.editText1);
-        GoButton = (Button)findViewById(R.id.button1);
+        GoButton = (ImageFilterButton) findViewById(R.id.button1);
         Browser = (WebView)findViewById(R.id.webView1);
         AppSettings = (ImageFilterButton)findViewById(R.id.settings);
-        // GoBack = (Button)findViewById(R.id.goBack);
+        Home = (ImageFilterButton)findViewById(R.id.gohome);
 
         Browser.loadUrl("https://www.google.com");
 
@@ -71,11 +71,10 @@ public class MainActivity extends Activity {
         });
 
 
-        AppSettings.setOnClickListener(new View.OnClickListener(){
+        /* AppSettings.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder alertDialog2 = new AlertDialog.Builder(
-                        MainActivity.this);
+                AlertDialog.Builder alertDialog2 = new AlertDialog.Builder(MainActivity.this);
 
                 alertDialog2.setTitle("App Information");
 
@@ -84,8 +83,19 @@ public class MainActivity extends Activity {
                                 "More functions will comming soon!"
                 );
 
+                AlertDialog.Builder alertDialog3 = new AlertDialog.Builder(MainActivity.this);
+
+                alertDialog3.setTitle("App Information");
+
+                alertDialog3.setMessage(
+                        "To update the browser, visit the GitHub page of this browser. To get to the page, click on 'Visit'"
+
+                );
+
                 alertDialog2.setIcon(R.drawable.full_icon);
-                alertDialog2.setNeutralButton("Ok",
+                alertDialog3.setIcon(R.drawable.full_icon);
+
+                alertDialog2.setPositiveButton("Ok",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 // Write your code here to execute after dialog
@@ -95,24 +105,29 @@ public class MainActivity extends Activity {
                             }
                         });
 
-                AlertDialog.Builder alertDialog3 = new AlertDialog.Builder(
-                        MainActivity.this);
-                alertDialog3.setTitle("Update your Browser");
-
-                alertDialog3.setMessage(
-                        "If you want to update your browser, visit following page: \n" +
-                                ""
-                );
-
-                // Setting Negative "NO" Btn
-                alertDialog2.setNegativeButton("Continue",
+                alertDialog2.setNeutralButton("Continue",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 // Write your code here to execute after dialog
-                                Toast.makeText(getApplicationContext(),
-                                        "Pog", Toast.LENGTH_SHORT)
-                                        .show();
+                                alertDialog3.show();
+                            }
+                        });
+
+
+                alertDialog3.setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Write your code here to execute after dialog
                                 dialog.cancel();
+                            }
+                        });
+
+                alertDialog3.setNeutralButton("Visit",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Write your code here to execute after dialog
+                                dialog.cancel();
+                                Browser.loadUrl("https://github.com/Juliaaan2502/simpleWebBrowserAndroid.Java/releases");
                             }
                         });
 
@@ -120,12 +135,17 @@ public class MainActivity extends Activity {
 
                 alertDialog2.show();
             }
-        });
+        }); */
 
 
         // Browser Settings
-        // AppSettings.setOnClickListener(v -> setContentView(R.layout.settings));
+        AppSettings.setOnClickListener(v -> setContentView(R.layout.settings));
         Browser.setOnClickListener(v -> setContentView(R.layout.settings));
+        Home.setOnClickListener(v -> Browser.loadUrl("https://google.com"));
+    }
+
+    public void goToHome(View v) {
+        setContentView(R.layout.settings);
     }
 
 }
