d3e1fa81f06dba356d9477d14080289ce430dba0
Julian <81520713+Juliaaan2502@users.noreply.github.com>
2021-07-09 21:50:23 +0200

    Add files via upload

--- app/build/generated/source/buildConfig/debug/eu/discordlistforbotsdevel/browser/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/eu/discordlistforbotsdevel/browser/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "eu.discordlistforbotsdevel.browser";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.2.1";
+  public static final String VERSION_NAME = "1.3";
 }
