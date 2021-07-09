3e2665e9622cdf9c423707e9ddef58ed9845905d
Julian <81520713+Juliaaan2502@users.noreply.github.com>
2021-07-09 22:03:49 +0200

    Add files via upload

--- app/src/main/java/eu/discordlistforbotsdevel/browser/MainActivity.java
+++ app/src/main/java/eu/discordlistforbotsdevel/browser/MainActivity.java
@@ -1,25 +1,16 @@
 package eu.discordlistforbotsdevel.browser;
 
-import androidx.constraintlayout.utils.widget.ImageFilterButton;
-import androidx.fragment.app.DialogFragment;
-
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.view.View.OnClickListener;
-import android.webkit.WebSettings;
-import android.webkit.WebViewClient;
-import android.widget.EditText;
-import android.webkit.WebView;
-import android.widget.Button;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import android.view.View.OnClickListener;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.constraintlayout.utils.widget.ImageFilterButton;
 
 public class MainActivity extends Activity {
 
@@ -28,11 +19,11 @@ public class MainActivity extends Activity {
     WebView Browser;
     ImageFilterButton AppSettings;
     ImageFilterButton Home;
+    Button GoBack;
+    Button Update;
 
+    private static class MyWebViewClient extends WebViewClient implements OnClickListener {
 
-    String androidAppVersion = "1.2.3";
-
-    private class MyWebViewClient extends WebViewClient implements OnClickListener {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
@@ -43,7 +34,8 @@ public class MainActivity extends Activity {
         public void onClick(View v) {
 
         }
-   }
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,11 +48,20 @@ public class MainActivity extends Activity {
         Browser = (WebView)findViewById(R.id.webView1);
         AppSettings = (ImageFilterButton)findViewById(R.id.settings);
         Home = (ImageFilterButton)findViewById(R.id.gohome);
+        GoBack = (Button)findViewById(R.id.goBack);
+        Update = (Button)findViewById(R.id.update);
+
+        if (Browser.canGoBack()) {
+            Browser.goBack();
+        }
+
+
 
         Browser.loadUrl("https://www.google.com");
 
         Browser.getSettings().setJavaScriptEnabled(true);
-        Browser.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        Browser.getSettings().setAppCacheEnabled(true);
+        Browser.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
         Browser.getSettings().setSupportZoom(true);
         Browser.getSettings().supportMultipleWindows();
 
@@ -70,82 +71,32 @@ public class MainActivity extends Activity {
             Browser.loadUrl("https://"+URLText.getText().toString());
         });
 
+        Home.setOnClickListener(v -> {
+            Browser.setWebViewClient(new MyWebViewClient());
+            Browser.loadUrl("https://google.com");
+        });
 
-        /* AppSettings.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder alertDialog2 = new AlertDialog.Builder(MainActivity.this);
-
-                alertDialog2.setTitle("App Information");
-
-                alertDialog2.setMessage(
-                        "Android Browser v" + androidAppVersion + "\n" +
-                                "More functions will coming soon!"
-                );
-
-                AlertDialog.Builder alertDialog3 = new AlertDialog.Builder(MainActivity.this);
-
-                alertDialog3.setTitle("App Information");
-
-                alertDialog3.setMessage(
-                        "To update the browser, visit the GitHub page of this browser. To get to the page, click on 'Visit'"
-
-                );
-
-                alertDialog2.setIcon(R.drawable.full_icon);
-                alertDialog3.setIcon(R.drawable.full_icon);
-
-                alertDialog2.setPositiveButton("Ok",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Write your code here to execute after dialog
-                                Toast.makeText(getApplicationContext(),
-                                        "Thank you for using this Browser <3", Toast.LENGTH_SHORT)
-                                        .show();
-                            }
-                        });
-
-                alertDialog2.setNeutralButton("Continue",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Write your code here to execute after dialog
-                                alertDialog3.show();
-                            }
-                        });
-
-
-                alertDialog3.setPositiveButton("Ok",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Write your code here to execute after dialog
-                                dialog.cancel();
-                            }
-                        });
-
-                alertDialog3.setNeutralButton("Visit",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Write your code here to execute after dialog
-                                dialog.cancel();
-                                Browser.loadUrl("https://github.com/Juliaaan2502/simpleWebBrowserAndroid.Java/releases");
-                            }
-                        });
-
-
-
-                alertDialog2.show();
-            }
-        }); */
-
-
-        // Browser Settings
         AppSettings.setOnClickListener(v -> setContentView(R.layout.settings));
         Browser.setOnClickListener(v -> setContentView(R.layout.settings));
-        Home.setOnClickListener(v -> Browser.loadUrl("https://google.com"));
     }
 
-    public void goToHome(View v) {
-        setContentView(R.layout.settings);
+    @Override
+    protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+        super.onActivityResult(_requestCode, _resultCode, _data);
+
+        switch(_requestCode) {
+            default:
+                break;
+        }
     }
 
+    @Override
+    public void onBackPressed() {
+        if(Browser.canGoBack()) {
+            Browser.goBack();
+        }
+        else {
+            finish();
+        }
+    }
 }
